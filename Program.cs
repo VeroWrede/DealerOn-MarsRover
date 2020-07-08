1259519b54245abaab5465a89970feb64b888d9e
VWrede <vwrede@earthnetworks.com>
2020-07-08 18:48:18 -0400

    adding rover position input check

--- Program.cs
+++ Program.cs
@@ -7,42 +7,100 @@ namespace Mars_Rover
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome on Mars!\n");
-            bool validInput = false;
-            while (validInput == false)
+            //get grid size
+            Console.WriteLine("Welcome on Mars!\nEnter Grid bounds:");
+            var userInput = Console.ReadLine();
+            bool validInput = CheckUserInput(userInput);
+            if (!validInput)
             {
-                validInput = CheckUserInput();
+                return;
+            }
+            List<int> gridCoordinates = new List<int>();
+            foreach(string coordinate in userInput.Trim().Split(' '))
+            {
+                gridCoordinates.Add(int.Parse(coordinate));
+            }
+            
+            // get rover starting point
+            Console.WriteLine("Please enter the rovers starting point:");
+            var startingPoint = Console.ReadLine();
+            List<string> startingCoordinates = new List<string>();
+            foreach(string coordinate in startingPoint.Trim().Split(' '))
+            {
+                startingCoordinates.Add(coordinate);
+            }
+            bool validStartingPoint = CheckStartingPoint(startingCoordinates, gridCoordinates);
+            if(!validStartingPoint)
+            {
+                return;
             }
 
-            Console.WriteLine("valid user input!");
+            // get turning instructions
+            Console.WriteLine("Please enter movement instructions:");
+            var instructionInput = Console.ReadLine();
+            List<string> instructionOptions = new List<string>() {"L", "R", "M"};
+            List<string> instructions = new List<string>();
             
+
         }
 
-        static bool CheckUserInput()
+        static bool CheckStartingPoint(List<string> startingCoordinates, List<int> gridCoordinates)
         {
-            Console.WriteLine("Enter bounds of grid you'd like to explore:");
-            var userInput = Console.ReadLine();
-            int inputCount = 0;
-            foreach (string bound in userInput.Trim().Split(' '))
+            // only three commands
+            if (startingCoordinates.Count != 3)
             {
-                inputCount ++;
+                Console.WriteLine("Enter three starting point coordinates. Try again.");
+                return false;
+            }
+            // item 1 and 2 must be grid coordinates within the given grid
+            for (int i = 0; i < 2; i++)
+            {
                 try
                 {
-                    int.Parse(bound);
+                    int coordinate = int.Parse(startingCoordinates[i]);
+                    if (coordinate < 0 || coordinate > gridCoordinates[i])
+                    {
+                        Console.WriteLine("Rover not in Grid.");
+                        return false;
+                    }
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine($"input {bound} is not a number");
+                    Console.WriteLine("starting coordinates out of bounds");
+                    return false;
+                }
+            }
+            List<string> cardinals = new List<string>(){"N", "S", "E", "W"};
+            // third grid coordinate must be valid cardinal 
+            if(!cardinals.Contains(startingCoordinates[2].ToUpper()))
+            {
+                Console.WriteLine("Wrong direction of rover");
+                return false;
+            }
+            return true;
+        }
+
+        static bool CheckUserInput(string userInput)
+        {
+            int inputCount = 0;
+            foreach (string bound in userInput.Trim().Split(' '))
+            {
+                try
+                {
+                    int.Parse(bound);
+                    inputCount ++;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"input {bound} is not a number. \nTry again");
                     return false;
                 }
             }
             if (inputCount != 2)
             {
-                Console.WriteLine("Please enter only two Coordinates \n");
+                Console.WriteLine("Please enter two Coordinates \nTry again");
                 return false;
             }
-
-
             return true;
         }
     }
