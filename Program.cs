216617328c9986d381990cb4e689179a7e777cda
VWrede <vwrede@earthnetworks.com>
2020-07-08 13:19:27 -0400

    First Rover on Mars commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mars_Rover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome on Mars!\n");
+            bool validInput = false;
+            while (validInput == false)
+            {
+                validInput = CheckUserInput();
+            }
+
+            Console.WriteLine("valid user input!");
+            
+        }
+
+        static bool CheckUserInput()
+        {
+            Console.WriteLine("Enter bounds of grid you'd like to explore:");
+            var userInput = Console.ReadLine();
+            foreach (string bound in userInput.Trim().Split(' '))
+            {
+                try
+                {
+                    int.Parse(bound);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"input {bound} is not a number");
+                    return false;
+                }
+            }
+
+            
+            return true;
+        }
+    }
+}
