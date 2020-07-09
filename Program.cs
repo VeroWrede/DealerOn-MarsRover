ff51ce003d8a1436f4e248db26a7407877632ce0
VWrede <vwrede@earthnetworks.com>
2020-07-09 11:46:21 -0400

    adding intruction execution

--- Program.cs
+++ Program.cs
@@ -38,8 +38,122 @@ namespace Mars_Rover
 
             // get turning instructions
             Console.WriteLine("Please enter movement instructions:");
-            var instructionInput = Console.ReadLine();
+            var instructionInput = Console.ReadLine().ToUpper();
             bool validInstructions = CheckInstructions(instructionInput);
+            if (!validInstructions)
+            {
+                return;
+            }
+            
+            // grid coordinates
+            List<int> gridBorders = new List<int>();
+            foreach (string bound in userInput.Trim().Split(' '))
+            {
+                gridBorders.Add(int.Parse(bound));
+            }
+            // starting point already set as List<string> startingCoordinates
+
+            List<int> roverPosition = new List<int>() {int.Parse(startingCoordinates[0]), int.Parse(startingCoordinates[1])};
+
+            // instructions already set as var/string instructionInput
+
+            // moving rover
+            for (int i = 0; i < instructionInput.Length; i++)
+            {
+                // instruction is to move
+                if(Char.ToString(instructionInput[i]) == "M")
+                {
+                    Console.WriteLine("move");
+                    Console.WriteLine($"orientation {startingCoordinates[2]}");
+                    //rover looks north (up)
+                    if((startingCoordinates[2].ToUpper()) == "N")
+                    {
+                        roverPosition[1] = roverPosition[1] + 1;
+                        if(roverPosition[1] > gridBorders[1])
+                        {
+                            Console.WriteLine("Rover left surveilance grid.");
+                        }
+                    }
+                    else if ((startingCoordinates[2].ToUpper()) == "S")
+                    {
+                        roverPosition[1] = roverPosition[1] - 1;
+                        if (roverPosition[1] < 0)
+                        {
+                            Console.WriteLine("rover left surveilance grid.");
+                            return;
+                        }
+                    }
+                    else if ((startingCoordinates[2].ToUpper()) == "E")
+                    {
+                        roverPosition[0] = roverPosition[0] + 1;
+                        if(roverPosition[0] > gridBorders[0])
+                        {
+                            Console.WriteLine("Rover left surveilance grid.");
+                        }
+                    }
+                    else if ((startingCoordinates[2].ToUpper()) == "W")
+                    {
+                        roverPosition[0] = roverPosition[0] - 1;
+                        Console.WriteLine($"here {roverPosition[0]}");
+                        if (roverPosition[0] < 0)
+                        {
+                            Console.WriteLine("rover left surveilance grid.");
+                            return;
+                        }
+                    }
+                }
+                // intruction to turn right
+                if (Char.ToString(instructionInput[i]) == "R")
+                {
+                    Console.WriteLine("rigt");
+
+                    if ((startingCoordinates[2].ToUpper()) == "N")
+                    {
+                        startingCoordinates[2] = "E";
+                    }
+                    else if ((startingCoordinates[2].ToUpper()) == "E")
+                    {
+                        startingCoordinates[2] = "S";
+                    }
+                    else if ((startingCoordinates[2].ToUpper()) == "S")
+                    {
+                        startingCoordinates[2] = "W";
+                    }
+                    else if ((startingCoordinates[2].ToUpper()) == "W")
+                    {
+                        startingCoordinates[2] = "N";
+                    }
+                }
+                // instruction to turn left
+                if (Char.ToString(instructionInput[i]) == "L")
+                {
+                    Console.WriteLine("left");
+                    
+
+                    if ((startingCoordinates[2].ToUpper()) == "N")
+                    {
+                        Console.WriteLine("facing north");
+                        startingCoordinates[2] = "W";
+                        Console.WriteLine($"orientation in left {startingCoordinates[2].ToUpper()}");
+                    }
+                    else if ((startingCoordinates[2].ToUpper()) == "E")
+                    {
+                        startingCoordinates[2] = "N";
+                    }
+                    else if ((startingCoordinates[2].ToUpper()) == "S")
+                    {
+                        startingCoordinates[2] = "E";
+                    }
+                    else if ((startingCoordinates[2].ToUpper()) == "W")
+                    {
+                        startingCoordinates[2] = "S";
+                    }
+                }
+            }
+            
+            Console.WriteLine($"rover position x: {roverPosition[0]}");
+            Console.WriteLine($"rover position y: {roverPosition[1]}");
+            Console.WriteLine($"rover orientation: {startingCoordinates[2]}");
 
         }
 
@@ -49,8 +163,7 @@ namespace Mars_Rover
             List<string> instructions = new List<string>();
             for (int i = 0; i < instructionsInput.Length; i++)
             {
-                string letterToUpper = Char.ToString(instructionsInput[i]).ToUpper();
-                if (!instructionOptions.Contains(letterToUpper))
+                if (!instructionOptions.Contains(Char.ToString(instructionsInput[i])))
                 {
                     Console.WriteLine("Invalid instructions.");
                     return false;
