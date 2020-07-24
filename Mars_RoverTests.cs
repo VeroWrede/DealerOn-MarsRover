cc352b2bd9245c255073c7941d8712013df2b1ab
VWrede <vwrede@earthnetworks.com>
2020-07-24 16:38:01 -0400

    Cleanup first test

--- Mars_RoverTests.cs
+++ Mars_RoverTests.cs
@@ -6,24 +6,18 @@ namespace Mars_Rover.UnitTests
     [TestFixture]
     public class ProgramTests
     {
-        private Program _program;
-
-        [SetUp]
-        public void SetUp()
-        {
-            _program = new Program();
-        }
-
         [Test]
         public void TestParseRepeat()
         {
-            bool testYes = _program.ParseRepeat("Y");
-            bool testNo = _program.ParseRepeat("gu");
-            bool testEmpty = _program.ParseRepeat("");
+            bool testYes = Program.ParseRepeat("Y");
+            bool testNo = Program.ParseRepeat("gu");
+            bool testEmpty = Program.ParseRepeat("");
 
             Assert.IsTrue(testYes);
             Assert.IsFalse(testNo);
             Assert.IsFalse(testEmpty);
         }
+
+
     }
 }
